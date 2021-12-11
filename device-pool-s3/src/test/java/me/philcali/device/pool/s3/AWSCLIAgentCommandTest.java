ca569e50d557fafb5619513ac7b2c9a886a45365
Philip <philip.cali@gmail.com>
2021-12-11 16:44:03 -0500

    chore: adding s3 tests

---
+++ device-pool-s3/src/test/java/me/philcali/device/pool/s3/AWSCLIAgentCommandTest.java
@@ -0,0 +1,48 @@
+package me.philcali.device.pool.s3;
+
+import me.philcali.device.pool.model.CommandInput;
+import me.philcali.device.pool.model.CopyInput;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith({MockitoExtension.class})
+class AWSCLIAgentCommandTest {
+
+    private AgentCommand command;
+    private Path temp;
+
+    @BeforeEach
+    void setup() throws IOException {
+        temp = Files.createTempDirectory("some-prefix");
+        command = AWSCLIAgentCommand.create();
+    }
+
+    @AfterEach
+    void teardown() throws IOException {
+        Files.deleteIfExists(temp);
+    }
+
+    @Test
+    void GIVEN_agent_command_is_initialized_WHEN_copy_THEN_command_input_is_provided() throws IOException {
+        CommandInput input = command.copy(CopyInput.builder()
+                .source("s3://bucket/some/path/file.txt")
+                .destination(temp.toString())
+                .build());
+
+        CommandInput expectedInput = CommandInput.builder()
+                .line("aws")
+                .addArgs("s3", "cp", "s3://bucket/some/path/file.txt", temp.toString())
+                .build();
+
+        assertEquals(expectedInput, input);
+    }
+}
---
+++ device-pool-s3/src/test/java/me/philcali/device/pool/s3/ContentTransferAgentFactoryS3Test.java
@@ -0,0 +1,55 @@
+package me.philcali.device.pool.s3;
+
+import me.philcali.device.pool.connection.Connection;
+import me.philcali.device.pool.content.ContentTransferAgent;
+import me.philcali.device.pool.content.ContentTransferAgentFactory;
+import me.philcali.device.pool.model.Host;
+import me.philcali.device.pool.model.PlatformOS;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith({MockitoExtension.class})
+class ContentTransferAgentFactoryS3Test {
+    @Mock
+    private Connection connection;
+    @Mock
+    private S3Client s3;
+    private Host host;
+    private ContentTransferAgentFactory factory;
+    private final String bucketName = "testBucket";
+
+    @BeforeEach
+    void setup() {
+        host = Host.builder()
+                .hostName("myhost.com")
+                .deviceId("i-abcabcabc")
+                .platform(PlatformOS.of("Linux", "armv7"))
+                .port(22)
+                .build();
+        factory = ContentTransferAgentFactoryS3.builder()
+                .bucketName(bucketName)
+                .s3(s3)
+                .build();
+    }
+
+    @Test
+    void GIVEN_factory_is_created_WHEN_connecting_to_a_host_THEN_agent_is_returned() {
+        ContentTransferAgent agent = factory.connect("abc-123", connection, host);
+
+        ContentTransferAgent expectedAgent = ContentTransferAgentS3.builder()
+                .bucketName(bucketName)
+                .prefix("abc-123/i-abcabcabc")
+                .command(AWSCLIAgentCommand.create())
+                .s3(s3)
+                .connection(connection)
+                .build();
+
+        assertEquals(expectedAgent, agent);
+    }
+}
