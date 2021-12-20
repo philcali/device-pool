f909cff9fe0a230ba4ba366ee2050a5e4418f25a
Philip <philip.cali@gmail.com>
2021-12-20 13:39:45 -0500

    chore: adding support for v1 and v2 infra

--- device-pool-service/device-pool-service-backend/src/main/java/me/philcali/device/pool/service/module/DevicePoolsComponent.java
+++ device-pool-service/device-pool-service-backend/src/main/java/me/philcali/device/pool/service/module/DevicePoolsComponent.java
@@ -1,8 +1,6 @@
 package me.philcali.device.pool.service.module;
 
 import com.amazonaws.serverless.proxy.jersey.JerseyLambdaContainerHandler;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import dagger.Component;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -15,7 +13,7 @@ import javax.inject.Singleton;
 })
 @Singleton
 public interface DevicePoolsComponent {
-    JerseyLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler();
+    JerseyLambdaContainerHandler handler();
 
     ResourceConfig application();
 }
--- device-pool-service/device-pool-service-backend/src/main/java/me/philcali/device/pool/service/module/ServiceContainerModule.java
+++ device-pool-service/device-pool-service-backend/src/main/java/me/philcali/device/pool/service/module/ServiceContainerModule.java
@@ -1,21 +1,26 @@
 package me.philcali.device.pool.service.module;
 
 import com.amazonaws.serverless.proxy.jersey.JerseyLambdaContainerHandler;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import dagger.Module;
 import dagger.Provides;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.inject.Singleton;
+import java.util.Optional;
 
 @Module
 class ServiceContainerModule {
+    private static final String API_VERSION = "API_VERSION";
+
     @Provides
     @Singleton
-    static JerseyLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler(ResourceConfig application) {
-        return JerseyLambdaContainerHandler.getHttpApiV2ProxyHandler(application);
+    static JerseyLambdaContainerHandler handler(ResourceConfig application) {
+        if (Optional.ofNullable(System.getenv(API_VERSION)).orElse("V1").equals("V1")) {
+            return JerseyLambdaContainerHandler.getAwsProxyHandler(application);
+        } else {
+            return JerseyLambdaContainerHandler.getHttpApiV2ProxyHandler(application);
+        }
     }
 
     @Provides
