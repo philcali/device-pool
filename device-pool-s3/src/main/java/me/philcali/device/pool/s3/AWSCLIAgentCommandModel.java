ff063d94d702f3a16a588e2319317fd81d137147
Philip Cali <philip.cali@gmail.com>
2022-01-23 18:07:11 -0500

    chore: adding license automation

--- device-pool-s3/src/main/java/me/philcali/device/pool/s3/AWSCLIAgentCommandModel.java
+++ device-pool-s3/src/main/java/me/philcali/device/pool/s3/AWSCLIAgentCommandModel.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022 Philip Cali
+ * Released under Apache-2.0 License
+ *     (https://www.apache.org/licenses/LICENSE-2.0)
+ */
+
 package me.philcali.device.pool.s3;
 
 import me.philcali.device.pool.model.ApiModel;
