dab654d067891e7a0ff053f1366b4d8aada9dd79
Philip Cali <philip.cali@gmail.com>
2022-02-13 19:24:12 -0500

    chore: adding more javadoc

--- device-pool-api/src/main/java/me/philcali/device/pool/provision/ProvisionService.java
+++ device-pool-api/src/main/java/me/philcali/device/pool/provision/ProvisionService.java
@@ -10,8 +10,30 @@ import me.philcali.device.pool.exceptions.ProvisioningException;
 import me.philcali.device.pool.model.ProvisionInput;
 import me.philcali.device.pool.model.ProvisionOutput;
 
+/**
+ * The injection point for a {@link me.philcali.device.pool.DevicePool} specific to the
+ * provisioning workflow or {@link me.philcali.device.pool.Device} acquisition. The
+ * acquisition of {@link me.philcali.device.pool.Device} can be independent of a
+ * reservation, which is to say: the intent to create data paths is decoupled from
+ * the data paths established in the provisioning process. This is strictly control
+ * plane interaction.
+ */
 public interface ProvisionService extends AutoCloseable {
+    /**
+     * Initializes the provisioning workflow with a {@link ProvisionInput}.
+     *
+     * @param input The initial provision request in the form of a {@link ProvisionInput}
+     * @return The provision request handle in the form of a {@link ProvisionOutput}
+     * @throws ProvisioningException Failure to create a provision request
+     */
     ProvisionOutput provision(ProvisionInput input) throws ProvisioningException;
 
+    /**
+     * Describes the provision request details.
+     *
+     * @param output A partial or full {@link ProvisionOutput}
+     * @return A partial or complete {@link ProvisionOutput}
+     * @throws ProvisioningException Failure to describe a provision request
+     */
     ProvisionOutput describe(ProvisionOutput output) throws ProvisioningException;
 }
--- device-pool-api/src/main/java/me/philcali/device/pool/reservation/ReservationService.java
+++ device-pool-api/src/main/java/me/philcali/device/pool/reservation/ReservationService.java
@@ -10,6 +10,23 @@ import me.philcali.device.pool.exceptions.ReservationException;
 import me.philcali.device.pool.model.Host;
 import me.philcali.device.pool.model.Reservation;
 
+/**
+ * The injection point for describing data path details for a provision request.
+ * The provisioning workflow will initialize reservation details for the intent
+ * of establishing data paths. The {@link ReservationService} exchanges initialized
+ * {@link Reservation} details for complete data paths. The data path is materialized
+ * in the form of a {@link Host} to facilitate reachability in various data plane
+ * components, namely the {@link me.philcali.device.pool.connection.Connection} and
+ * {@link me.philcali.device.pool.content.ContentTransferAgent}.
+ */
 public interface ReservationService extends AutoCloseable {
+    /**
+     * The exchange of a {@link Reservation} manifest for a partial or
+     * complete data path in the form of a {@link Host} entry.
+     *
+     * @param reservation The data path metadata in the form of a {@link Reservation}
+     * @return The partial or complete data path details in the form of a {@link Host}
+     * @throws ReservationException Failure to exchange {@link Reservation} metadata
+     */
     Host exchange(Reservation reservation) throws ReservationException;
 }
