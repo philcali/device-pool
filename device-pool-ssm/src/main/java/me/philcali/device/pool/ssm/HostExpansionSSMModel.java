f018dacdaaba951a2ae42a5a3f8d4b1b76707b4a
Philip Cali <philip.cali@gmail.com>
2022-04-09 15:10:08 -0400

    chore: moving common settings in mixin

--- device-pool-ssm/src/main/java/me/philcali/device/pool/ssm/HostExpansionSSMModel.java
+++ device-pool-ssm/src/main/java/me/philcali/device/pool/ssm/HostExpansionSSMModel.java
@@ -8,62 +8,22 @@ package me.philcali.device.pool.ssm;
 
 import me.philcali.device.pool.model.ApiModel;
 import me.philcali.device.pool.model.Host;
-import me.philcali.device.pool.model.PlatformOS;
 import me.philcali.device.pool.provision.ExpandingHostProvider;
 import org.immutables.value.Value;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.DescribeInstanceInformationRequest;
 import software.amazon.awssdk.services.ssm.model.DescribeInstanceInformationResponse;
-import software.amazon.awssdk.services.ssm.model.InstanceInformation;
-import software.amazon.awssdk.services.ssm.model.InstanceInformationStringFilter;
 import software.amazon.awssdk.services.ssm.model.PingStatus;
 
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 @ApiModel
 @Value.Immutable
-abstract class HostExpansionSSMModel implements ExpandingHostProvider.ExpansionFunction {
-
+abstract class HostExpansionSSMModel implements ExpandingHostProvider.ExpansionFunction, HostProvisionMixin {
     @Value.Default
     SsmClient ssm() {
         return SsmClient.create();
-    }
-
-    abstract String architecture();
-
-    @Value.Default
-    BiFunction<InstanceInformation, Host.Builder, Host.Builder> hostBuilder() {
-        return (instance, builder) -> builder
-                .port(22)
-                .hostName(instance.ipAddress())
-                .deviceId(instance.instanceId())
-                .platform(PlatformOS.of(instance.platformType().toString(), architecture()));
-    }
-
-    @Nullable
-    abstract String poolId();
-
-    @Value.Default
-    Collection<InstanceInformationStringFilter> filters() {
-        final List<InstanceInformationStringFilter> filters = new ArrayList<>();
-        if (poolId() != null) {
-            filters.add(InstanceInformationStringFilter.builder()
-                    .key("tag:DevicePool")
-                    .values(poolId())
-                    .build());
-        } else {
-            filters.add(InstanceInformationStringFilter.builder()
-                    .key("PingStatus")
-                    .values(PingStatus.ONLINE.toString())
-                    .build());
-        }
-        return Collections.unmodifiableList(filters);
     }
 
     @Override
--- device-pool-ssm/src/main/java/me/philcali/device/pool/ssm/HostProviderSSM.java
+++ device-pool-ssm/src/main/java/me/philcali/device/pool/ssm/HostProviderSSM.java
@@ -7,26 +7,14 @@
 package me.philcali.device.pool.ssm;
 
 import me.philcali.device.pool.model.APIShadowModel;
-import me.philcali.device.pool.model.Host;
-import me.philcali.device.pool.model.PlatformOS;
 import me.philcali.device.pool.provision.ExpandingHostProvider;
 import org.immutables.value.Value;
 import software.amazon.awssdk.services.ssm.SsmClient;
-import software.amazon.awssdk.services.ssm.model.InstanceInformation;
-import software.amazon.awssdk.services.ssm.model.InstanceInformationStringFilter;
-import software.amazon.awssdk.services.ssm.model.PingStatus;
-
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.BiFunction;
 
 @APIShadowModel
 @Value.Immutable
 @Deprecated
-public abstract class HostProviderSSM extends ExpandingHostProvider {
+public abstract class HostProviderSSM extends ExpandingHostProvider implements HostProvisionMixin {
     private static final int DEFAULT_CACHE_THRESHOLD = 100;
 
     abstract SsmClient ssm();
@@ -58,37 +46,6 @@ public abstract class HostProviderSSM extends ExpandingHostProvider {
                 .filters(filters())
                 .hostBuilder(hostBuilder())
                 .build();
-    }
-
-    abstract String architecture();
-
-    @Value.Default
-    BiFunction<InstanceInformation, Host.Builder, Host.Builder> hostBuilder() {
-        return (instance, builder) -> builder
-                .port(22)
-                .hostName(instance.ipAddress())
-                .deviceId(instance.instanceId())
-                .platform(PlatformOS.of(instance.platformType().toString(), architecture()));
-    }
-
-    @Nullable
-    abstract String poolId();
-
-    @Value.Default
-    Collection<InstanceInformationStringFilter> filters() {
-        final List<InstanceInformationStringFilter> filters = new ArrayList<>();
-        if (poolId() != null) {
-            filters.add(InstanceInformationStringFilter.builder()
-                    .key("tag:DevicePool")
-                    .values(poolId())
-                    .build());
-        } else {
-            filters.add(InstanceInformationStringFilter.builder()
-                    .key("PingStatus")
-                    .values(PingStatus.ONLINE.toString())
-                    .build());
-        }
-        return Collections.unmodifiableList(filters);
     }
 
     @Override
